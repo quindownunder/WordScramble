e347e839fbb962078ef2d736fbc60418aaf4904d
Matthew Richardson <mat_richardson@mac.com>
2020-08-12 16:40:52 +1000

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Matthew Richardson on 12/8/20.
+//  Copyright © 2020 Matthew Richardson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
